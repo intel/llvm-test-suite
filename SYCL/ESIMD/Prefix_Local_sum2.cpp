f16b23e01bb9432452877d7dd736b3765aeb0566
kychendev <59320499+kychendev@users.noreply.github.com>
2020-10-11 13:04:10 -0700

    Update SYCL/ESIMD/Prefix_Local_sum2.cpp
    Co-authored-by: vladimirlaz <vladimir.lazarev@intel.com>

--- SYCL/ESIMD/Prefix_Local_sum2.cpp
+++ SYCL/ESIMD/Prefix_Local_sum2.cpp
@@ -8,7 +8,7 @@
 // TODO enable on Windows and Level Zero
 // REQUIRES: linux && gpu && opencl
 // RUN: %clangxx-esimd -fsycl %s -o %t.out
-// RUN: env SYCL_DEVICE_TYPE=HOST %t.out 20
+// RUN: HOST_RUN_PLACEHOLDER %t.out 20
 // RUN: %ESIMD_RUN_PLACEHOLDER %t.out 20
 
 #include "esimd_test_utils.hpp"
