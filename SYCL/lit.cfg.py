d5cf67732c55c13d0d068c982e414cb97824b099
Vladimir Lazarev <vladimir.lazarev@intel.com>
2020-12-29 10:39:39 +0300

    Fix misprint

--- SYCL/lit.cfg.py
+++ SYCL/lit.cfg.py
@@ -94,7 +94,7 @@ if config.extra_environment:
 config.substitutions.append( ('%sycl_libs_dir',  config.sycl_libs_dir ) )
 config.substitutions.append( ('%sycl_include',  config.sycl_include ) )
 
-if lit_config.params.get('gpu-intel-dg1', False)
+if lit_config.params.get('gpu-intel-dg1', False):
     config.available_features.add('gpu-intel-dg1')
 
 # check if compiler supports CL command line options
