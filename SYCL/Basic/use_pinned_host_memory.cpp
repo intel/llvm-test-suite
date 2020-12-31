9e3806fca4955bb64a0ef50e7ae49a5ebe1e9846
rbegam <rehana.begam@intel.com>
2020-12-31 08:56:11 -0800

    created a separate test.
    Signed-off-by: rbegam <rehana.begam@intel.com>

--- SYCL/Basic/use_pinned_host_memory.cpp
+++ SYCL/Basic/use_pinned_host_memory.cpp
@@ -1,19 +1,9 @@
-// RUN: %clangxx %s -o %t1.out -lsycl -I %sycl_include
-// RUN: %RUN_ON_HOST %t1.out
-// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t2.out
-// RUN: env SYCL_PI_TRACE=2 ZE_DEBUG=1 %GPU_RUN_PLACEHOLDER %t2.out 2>&1 %GPU_CHECK_PLACEHOLDER
-
-// RUN: %RUN_ON_HOST %t2.out
-// RUN: %CPU_RUN_PLACEHOLDER %t2.out
-// RUN: %GPU_RUN_PLACEHOLDER %t2.out
-// RUN: %ACC_RUN_PLACEHOLDER %t2.out
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+// RUN: env SYCL_PI_TRACE=2 %CPU_RUN_PLACEHOLDER %t.out 2>&1 %CPU_CHECK_PLACEHOLDER
 
 #include <CL/sycl.hpp>
 
-#include <cassert>
 #include <string>
-
-using namespace cl::sycl;
 
 int main() {
   const sycl::range<1> N{1};
@@ -31,27 +21,6 @@ int main() {
     auto Acc = Buf.get_access<sycl::access::mode::read_write>(CGH);
     CGH.single_task<class init_a>([=]() {});
   });
-
-  {
-    int data1[10] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-    {
-      buffer<int, 1> a(data1, range<1>(10), {property::buffer::use_host_ptr()});
-      buffer<int, 1> b(
-          range<1>(10),
-          {ext::oneapi::property::buffer::use_pinned_host_memory()});
-      queue myQueue;
-      myQueue.submit([&](handler &cgh) {
-        auto A = a.get_access<access::mode::read_write>(cgh);
-        auto B = b.get_access<access::mode::read_write>(cgh);
-        cgh.parallel_for<class init_b>(range<1>{10}, [=](id<1> index) {
-          B[index] = 0;
-          A[index] = B[index] + 1;
-        });
-      });
-    } // Data is copied back because there is a user side shared_ptr
-    for (int i = 0; i < 10; i++)
-      assert(data1[i] == 1);
-  }
 
   try {
     int Data = 0;
@@ -74,4 +43,3 @@ int main() {
 // CHECK:---> piMemBufferCreate
 // CHECK-NEXT: {{.*}} : {{.*}}
 // CHECK-NEXT: {{.*}} : 17
-// CHECK:ZE ---> zeMemAllocHost
