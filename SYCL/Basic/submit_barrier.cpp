f128bec4904bae4e104928cb9e7dd48059f6c926
Sergey V Maslov <sergey.v.maslov@intel.com>
2021-01-21 14:55:12 -0800

    use default device selector
    Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>

--- SYCL/Basic/submit_barrier.cpp
+++ SYCL/Basic/submit_barrier.cpp
@@ -1,4 +1,4 @@
-// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
+// RUN: %clangxx -fsycl %s -o %t.out
 // RUN: %HOST_RUN_PLACEHOLDER %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
@@ -7,8 +7,12 @@
 #include <stdlib.h>
 
 int main() {
-  cl::sycl::device dev;
+
+  cl::sycl::device dev{cl::sycl::default_selector{}};
   cl::sycl::queue q{dev};
+
+  q.submit(
+      [&](sycl::handler &cgh) { cgh.single_task<class kernel1>([]() {}); });
 
   cl::sycl::event e = q.submit_barrier();
   e.wait_and_throw();
