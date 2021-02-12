d27ab0d70c8bc5cd515a61bb7d522020a7ab62b9
Fedor Veselovsky <fedor.veselovsky@intel.com>
2021-02-12 15:30:48 +0300

    cosmetic fix

--- SYCL/ESIMD/spec_const/spec_const_int64.cpp
+++ SYCL/ESIMD/spec_const/spec_const_int64.cpp
@@ -1,4 +1,4 @@
-//==--------------- spec_const_long.cpp  - DPC++ ESIMD on-device test -----===//
+//==-------------- spec_const_int64.cpp  - DPC++ ESIMD on-device test -----===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
