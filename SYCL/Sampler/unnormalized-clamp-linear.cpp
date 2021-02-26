95aefc42facfef311515d1627d8bf11ad87a2e87
Chris Perkins <chris.perkins@intel.com>
2021-02-26 11:32:24 -0800

    adding run directives and xfail for non-working combinations
    Signed-off-by: Chris Perkins <chris.perkins@intel.com>

--- SYCL/Sampler/unnormalized-clamp-linear.cpp
+++ SYCL/Sampler/unnormalized-clamp-linear.cpp
@@ -1,5 +1,9 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: %HOST_RUN_PLACEHOLDER %t.out %HOST_CHECK_PLACEHOLDER
+// RUN: %CPU_RUN_PLACEHOLDER %t.out %CPU_CHECK_PLACEHOLDER
+// RUN: %GPU_RUN_PLACEHOLDER %t.out %GPU_CHECK_PLACEHOLDER
+// XFAIL: gpu && (level_zero || opencl)
+// XFAIL: cpu
 
 // GPU does not correctly interpolate when using clamp.  Waiting on fix.
 // Both OCL and LevelZero have this issue.
