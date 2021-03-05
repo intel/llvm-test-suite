0a01ee8887b2479cfbe3783f05435ecfa7117122
John Pennycook <john.pennycook@intel.com>
2021-03-05 10:53:14 -0500

    [SYCL] Move extra CUDA arg to correct line
    Signed-off-by: John Pennycook <john.pennycook@intel.com>

--- SYCL/AtomicRef/add-native.cpp
+++ SYCL/AtomicRef/add-native.cpp
@@ -1,7 +1,8 @@
 // RUN: %clangxx -fsycl -fsycl-unnamed-lambda -DSYCL_USE_NATIVE_FP_ATOMICS \
-// RUN: -fsycl-targets=%sycl_triple %s -o %t.out
+// RUN: -fsycl-targets=%sycl_triple %s -o %t.out \
+// RUN: -Xsycl-target-backend=nvptx64-nvidia-cuda-sycldevice --cuda-gpu-arch=sm_60
 // RUN: %HOST_RUN_PLACEHOLDER %t.out
-// RUN: %GPU_RUN_PLACEHOLDER %t.out -Xsycl-target-backend=nvptx64-nvidia-cuda-sycldevice --cuda-gpu-arch=sm_60
+// RUN: %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // TODO: Remove items from UNSUPPORTED once corresponding backends support
 // "native" implementation
