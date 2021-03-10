c33c487945a76e7af912672bb18c2f57ea7f47a5
Kai Yu Chen <kai.yu.chen@intel.com>
2021-03-10 12:59:54 -0800

    [SYCL][ESIMD] Resolve [S,s]tencil* sporadic test error.

--- SYCL/ESIMD/stencil2.cpp
+++ SYCL/ESIMD/stencil2.cpp
@@ -24,7 +24,7 @@
 // test 8x16 block size
 //
 #define DIM_SIZE (1 << 13)
-#define SQUARE_SZ (DIM_SIZE * DIM_SIZE)
+#define SQUARE_SZ (DIM_SIZE * DIM_SIZE + 1)
 
 #define WIDTH 16
 #define HEIGHT 16
