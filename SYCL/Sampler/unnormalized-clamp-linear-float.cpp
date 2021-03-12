e8be63cb0a4dde4c98da18821046753f0abc4a85
Chris Perkins <chris.perkins@intel.com>
2021-03-12 08:58:11 -0800

    new issue for L0 and Win discovered. marking xfail for now

--- SYCL/Sampler/unnormalized-clamp-linear-float.cpp
+++ SYCL/Sampler/unnormalized-clamp-linear-float.cpp
@@ -3,9 +3,13 @@
 // RUN: %CPU_RUN_PLACEHOLDER %t.out %CPU_CHECK_PLACEHOLDER
 // RUN: %GPU_RUN_PLACEHOLDER %t.out %GPU_CHECK_PLACEHOLDER
 // XFAIL: cuda
+// XFAIL: level_zero && windows
 
 // CUDA works with image_channel_type::fp32, but not with any 8-bit per channel
 // type (such as unorm_int8)
+
+// On Windows, LevelZero returns wrong result for outside right pixel when using
+// unorm_int8 data.
 
 /*
     This file sets up an image, initializes it with data,
