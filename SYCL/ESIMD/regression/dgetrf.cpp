636197fc05cf0fb1019e41099e0cf22fbc5f9e8f
Chuvelev, Michael <michael.chuvelev@intel.com>
2021-03-16 11:33:33 -0700

    copyright changed

--- SYCL/ESIMD/regression/dgetrf.cpp
+++ SYCL/ESIMD/regression/dgetrf.cpp
@@ -1,16 +1,10 @@
-/*******************************************************************************
- * Copyright 2021 Intel Corporation.
- *
- * This software and the related documents are Intel copyrighted  materials, and
- * your use of  them is  governed by the  express license  under which  they
- *were provided to you (License).  Unless the License provides otherwise, you
- *may not use, modify, copy, publish, distribute,  disclose or transmit this
- *software or the related documents without Intel's prior written permission.
- *
- * This software and the related documents  are provided as  is,  with no
- *express or implied  warranties,  other  than those  that are  expressly stated
- *in the License.
- *******************************************************************************/
+//==-------------- dgetrf.cpp  - DPC++ ESIMD on-device test ----------------==//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 // REQUIRES: gpu
 // UNSUPPORTED: cuda
 // RUN: %clangxx-esimd -fsycl -DUSE_REF %s -I%S/.. -o %t.ref.out
