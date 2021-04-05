43838cb15abc6b3109598162d2636ab11e8c83ba
Fedor Veselovsky <fedor.veselovsky@intel.com>
2021-04-05 22:27:32 +0300

    add comment

--- SYCL/ESIMD/fp_call_recursive.cpp
+++ SYCL/ESIMD/fp_call_recursive.cpp
@@ -6,7 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 // REQUIRES: gpu
-// Recursion is not supported in ESIMD (PR#3390)
+// Recursion is not supported in ESIMD (intel/llvm PR#3390)
 // REQUIRES: TEMPORARY_DISBLED
 // UNSUPPORTED: cuda
 // RUN: %clangxx -Xclang -fsycl-allow-func-ptr -fsycl %s -o %t.out
