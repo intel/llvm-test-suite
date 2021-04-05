f12fcd8db12979d1a6926eadfa2ad4194d2bab36
Sergey Semenov <sergey.semenov@intel.com>
2021-04-05 17:53:13 +0300

    Simplify the test

--- SYCL/GroupLocalMemory/no_early_opt.cpp
+++ SYCL/GroupLocalMemory/no_early_opt.cpp
@@ -24,39 +24,30 @@ using namespace sycl;
 int main() {
   queue Q;
   {
-    std::vector<int> VecA(Size, 0);
-    std::vector<int> VecB(Size, 0);
-    buffer<int, 1> BufA{VecA.data(), range<1>(Size)};
-    buffer<int, 1> BufB{VecB.data(), range<1>(Size)};
+    std::vector<int *> VecA(Size, 0);
+    std::vector<int *> VecB(Size, 0);
+    buffer<int *, 1> BufA{VecA.data(), range<1>(Size)};
+    buffer<int *, 1> BufB{VecB.data(), range<1>(Size)};
 
     Q.submit([&](handler &Cgh) {
       auto AccA = BufA.get_access<access::mode::read_write>(Cgh);
       auto AccB = BufB.get_access<access::mode::read_write>(Cgh);
       Cgh.parallel_for<KernelA>(
           nd_range<1>(range<1>(Size), range<1>(WgSize)), [=](nd_item<1> Item) {
-            multi_ptr<int[WgSize], access::address_space::local_space> PtrA =
-                group_local_memory_for_overwrite<int[WgSize]>(Item.get_group());
-            multi_ptr<int[WgSize], access::address_space::local_space> PtrB =
-                group_local_memory_for_overwrite<int[WgSize]>(Item.get_group());
-            size_t LocalId = Item.get_local_linear_id();
-            (*PtrA)[LocalId] = 1;
-            (*PtrB)[LocalId] = 1;
-            (*PtrA)[LocalId] *= LocalId;
-            (*PtrB)[LocalId] *= LocalId;
-            Item.barrier();
+            multi_ptr<int, access::address_space::local_space> PtrA =
+                group_local_memory_for_overwrite<int>(Item.get_group());
+            multi_ptr<int, access::address_space::local_space> PtrB =
+                group_local_memory_for_overwrite<int>(Item.get_group());
 
-            size_t LocalIdx = LocalId ^ 1;
-            size_t GlobalIdx = Item.get_global_linear_id() ^ 1;
-            AccA[GlobalIdx] = (*PtrA)[LocalIdx];
-            AccB[GlobalIdx] = (*PtrB)[LocalIdx];
+            size_t GlobalId = Item.get_global_linear_id();
+            AccA[GlobalId] = PtrA;
+            AccB[GlobalId] = PtrB;
           });
     });
 
     auto AccA = BufA.get_access<access::mode::read>();
     auto AccB = BufB.get_access<access::mode::read>();
-    for (size_t I = 0; I < Size; ++I) {
-      assert(AccA[I] == I % WgSize);
-      assert(AccB[I] == I % WgSize);
-    }
+    for (size_t I = 0; I < Size; ++I)
+      assert(AccA[I] != AccB[I]);
   }
 }
