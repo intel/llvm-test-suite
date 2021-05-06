ac01a88341b05a1fac9929d1be624f63e8629c59
Vladimir Lazarev <vladimir.lazarev@intel.com>
2021-05-06 07:42:20 +0300

    Apply review comments

--- SYCL/ESIMD/noinline_args_size/noinline_args_char_int_size_96.cpp
+++ SYCL/ESIMD/noinline_args_size/noinline_args_char_int_size_96.cpp
@@ -8,7 +8,10 @@
 // REQUIRES: gpu
 // UNSUPPORTED: cuda
 // RUN: %clangxx -fsycl -I%S/.. %s -o %t.out
-// RUN: env IGC_FunctionControl=2 IGC_ForceInlineStackCallWithImplArg=1 %GPU_RUN_PLACEHOLDER %t.out
+// RUN: env IGC_FunctionControl=3 IGC_ForceInlineStackCallWithImplArg=1 %GPU_RUN_PLACEHOLDER %t.out
+
+// Regression in GPU RT 21.16.19610
+// XFAIL: linux
 
 #include <cstdint>
 
