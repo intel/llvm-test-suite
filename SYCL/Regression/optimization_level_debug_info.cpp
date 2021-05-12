42d23debabc23ac06b7870869887bc902c0f1cba
Steffen Larsen <steffen.larsen@codeplay.com>
2021-05-12 16:48:36 +0100

    Fix formatting
    Signed-off-by: Steffen Larsen <steffen.larsen@codeplay.com>

--- SYCL/Regression/optimization_level_debug_info.cpp
+++ SYCL/Regression/optimization_level_debug_info.cpp
@@ -9,17 +9,17 @@
 // RUN: %clangxx %debug_option -fsycl -fsycl-targets=%sycl_triple %s -O -o %t.out
 
 // NOTE: Tests that debugging information can be generated for all optimization
-// levels
+// levels.
 
 #include <CL/sycl.hpp>
 
 int main() {
-    sycl::queue q;
+  sycl::queue q;
 
-    q.submit([&](sycl::handler& cgh) {
-        cgh.parallel_for<class kernel_test>(100, [=](sycl::id<1> idx) { });
-    });
-    q.wait();
+  q.submit([&](sycl::handler &cgh) {
+    cgh.parallel_for<class kernel_test>(100, [=](sycl::id<1> idx) {});
+  });
+  q.wait();
 
-    return 0;
+  return 0;
 }
