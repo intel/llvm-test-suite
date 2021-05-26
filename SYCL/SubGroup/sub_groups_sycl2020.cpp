12fa46f4d280e150002861e0e12e180ddef2271d
Alexander Batashev <alexander.batashev@intel.com>
2021-05-26 13:08:32 +0300

    disable test on cuda

--- SYCL/SubGroup/sub_groups_sycl2020.cpp
+++ SYCL/SubGroup/sub_groups_sycl2020.cpp
@@ -1,6 +1,9 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
+//
+// TODO enable test on CUDA once kernel_bundle is supported
+// UNSUPPORTED: cuda
 
 #include <sycl/sycl.hpp>
 
