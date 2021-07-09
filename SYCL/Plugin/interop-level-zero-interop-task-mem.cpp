c1afbee61cf79ae1b52793f9c34d1442ad2c182c
Mikhail Nikolskiy <mikhail.y.nikolsky@intel.com>
2021-07-09 20:31:44 +0300

    clang-format

--- SYCL/Plugin/interop-level-zero-interop-task-mem.cpp
+++ SYCL/Plugin/interop-level-zero-interop-task-mem.cpp
@@ -33,9 +33,9 @@ int main() {
           cgh.interop_task([=](const interop_handler &ih) {
             void *device_ptr = ih.get_mem<backend::level_zero>(buffer_acc);
             ze_memory_allocation_properties_t memAllocProperties{};
-                        ze_result_t res = zeMemGetAllocProperties(ze_context, device_ptr, &memAllocProperties,
-                                                nullptr);
-                        assert(res == ZE_RESULT_SUCCESS);
+            ze_result_t res = zeMemGetAllocProperties(
+                ze_context, device_ptr, &memAllocProperties, nullptr);
+            assert(res == ZE_RESULT_SUCCESS);
 
             ze_image_handle_t ze_image =
                 ih.get_mem<backend::level_zero>(image_acc);
